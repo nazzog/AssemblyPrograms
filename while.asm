80f50741b60a6c759ed459dd86756e9f5fb43628
nazzog <tialvesm@gmail.com>
2017-05-12 23:24:53 -0300

    programa while add

---
+++ while.asm
@@ -0,0 +1,11 @@
+#Exercício 4: while (a != 0) { c = c + b; a--; }
+#Supor que as variáveis de a, b e c estão carregadas nos
+#registradores $8, $9 e $10
+
+.data
+while:
+    beq $8, $0, exit
+    add $10, $10, $9
+    addi $8, $8, -1
+    j while
+exit:
